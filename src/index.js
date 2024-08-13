a3ae1333bad8a5e801e1f6f1bc73a3aeec8f4026
vivi2004 <vivekpurbey586@gmail.com>
2024-08-13 23:58:55 +0530

    connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-ECHO is on.
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config();
+connectDB()
+
+
+/*
+ import express from "express";
+ const app  = express();
+
+ ( async()  => {
+         try {
+           mongoose.connect(`{process.env.MONGODN_URI}/{DB_NAME}`)  
+           app.on("error",(error) => {
+             console.log("ERROR" ,error);
+             throw error
+           })  
+
+    app.listen(process.env.PORT,()=>{
+       console.log( `App is listening on port $
+        {process.env.PORT}`);
+       
+    })
+
+
+   } catch (error) {
+      console.error("ERROR",error);
+      
+    }
+
+
+ })
+  */
+ 
+//  export default connectDB;
+
+
