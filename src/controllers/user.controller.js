44ac53a9228df61bfc0cb50815186d1d65f74b07
vivi2004 <vivekpurbey586@gmail.com>
2024-09-27 23:16:02 +0530

    pipeline  aggregation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,16 +5,17 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { response } from "express";
 
 const generateAccessAndRefreshToken = async (userId) => {
     try {
         const user = await User.findById(userId);
         const accessToken = user.generateAccessToken();
         const refreshToken = user.generateRefreshToken();
-        console.log("Generated Acess Token" , accessToken);
+        console.log("Generated Acess Token", accessToken);
         console.log("Generated Refresh Token ", refreshToken);
-        
-        
+
+
         // Store the refreshToken in MongoDB
         user.refreshToken = refreshToken;
         await user.save({ validateBeforeSave: false });
@@ -173,128 +174,205 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 });
 
 
-const changePassword = asyncHandler(async (req,res) => {
-   const { oldPassword, newPassword} =  req.body 
-    
-   const   user = await   User.findById(req.user?.id)
-  const isPasswordCorrect = await   user.isPasswordCorrect(oldPassword)
+const changePassword = asyncHandler(async (req, res) => {
+    const { oldPassword, newPassword } = req.body
 
-  if(!isPasswordCorrect) {
-    throw new apiError (400 ," Invalid old password");
-     
-  }   
-   user.password = newPassword
+    const user = await User.findById(req.user?.id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new apiError(400, " Invalid old password");
+
+    }
+    user.password = newPassword
 
 
-   await user.save({ validateBeforeSave: false });
+    await user.save({ validateBeforeSave: false });
     return res
-    .status(200)
-    .json(new apiResponse(200, {}, "Password changed successfully"))
- })
- const getCurrentUser =  asyncHandler(async (req, res)=>{
-      return res
-      .status(200)
-      .json(200, req.user, "Current user fetched successfully") 
- })
+        .status(200)
+        .json(new apiResponse(200, {}, "Password changed successfully"))
+})
+const getCurrentUser = asyncHandler(async (req, res) => {
+    return res
+        .status(200)
+        .json(200, req.user, "Current user fetched successfully")
+})
 
-const updateAccountDetails = asyncHandler( async(req,res) => {
-     const  { fullName,email}   = req.body
-     if(!fullName ||!email) {
-        throw new apiError (400, "All fields are required")
-     }
-//  information to  find and ,update fullName and email 
-   const user =  User.findByIdAndUpdate(
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body
+    if (!fullName || !email) {
+        throw new apiError(400, "All fields are required")
+    }
+    //  information to  find and ,update fullName and email 
+    const user = User.findByIdAndUpdate(
         req.user?.id,
-       {
-        // its function  mongodb to store  the value.
-   $set: {
-        fullName,
-        email,
-        // email:email   we can define these values in both ways it is correct 
-        }
+        {
+            // its function  mongodb to store  the value.
+            $set: {
+                fullName,
+                email,
+                // email:email   we can define these values in both ways it is correct 
+            }
 
         }
-       ,
-       {new: true } , // it is basically after update value is saved and showed to use...
-     ).select("-password")
+        ,
+        { new: true }, // it is basically after update value is saved and showed to use...
+    ).select("-password")
 
-     return res
-    .status(200)
-    .json(new apiResponse (200 , user, "Account details updated successfully" ))
+    return res
+        .status(200)
+        .json(new apiResponse(200, user, "Account details updated successfully"))
 
 })
- 
-    const updateUserAvatar  =  asyncHandler(async(req, res) => {
-        const avatarLocalPath =  req.file?.path
 
-        if(!avatarLocalPath) {
-            throw new apiError(400, "Avatar is missing ")   
-        }
-     
-     const avatar = await  uploadOnCloudinary (avatarLocalPath) 
-        
-      if(!avatar.url) {
-        throw new apiError (400, "Error while uploading  on avatar")
+const updateUserAvatar = asyncHandler(async (req, res) => {
+    const avatarLocalPath = req.file?.path
 
-      }
-     // updating the avatar   
-       const user  = await User.findByIdAndUpdate(
-            { 
+    if (!avatarLocalPath) {
+        throw new apiError(400, "Avatar is missing ")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if (!avatar.url) {
+        throw new apiError(400, "Error while uploading  on avatar")
+
+    }
+    // updating the avatar   
+    const user = await User.findByIdAndUpdate(
+        {
             $set: {
-              avatar:avatar.url     
-             }
-            } ,
-            {new: true}
-         ).select("-password")
+                avatar: avatar.url
+            }
+        },
+        { new: true }
+    ).select("-password")
 
 
-         return res
-         .status(200)
-         .json(
-            new  apiResponse( 200, user, " Avatar  updated sucessfully ");
+    return res
+        .status(200)
+        .json(
+            new apiResponse(200, user, " Avatar  updated sucessfully ");
          )
 
-    })  
+    })
 
-    const updateCoverImage  =  asyncHandler(async(req, res) => {
-        const coverImageLocalPath =  req.file?.path
+const updateCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path
 
-        if(!coverImageLocalPath ) {
-            throw new apiError(400, "coverImage  is missing ")   
-        }
-     
-     const coverImage= await  uploadOnCloudinary (coverImageLocalPath ) 
-        
-      if(!coverImage.url) {
-        throw new apiError (400, "Error while uploading  on coverImage ")
+    if (!coverImageLocalPath) {
+        throw new apiError(400, "coverImage  is missing ")
+    }
 
-      }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-     // updating the  coverImage  
-         const user =  await User.findByIdAndUpdate(
-            { 
+    if (!coverImage.url) {
+        throw new apiError(400, "Error while uploading  on coverImage ")
+
+    }
+
+    // updating the  coverImage  
+    const user = await User.findByIdAndUpdate(
+        {
             $set: {
-                coverImage:coverImage.url     
-             }
-            } ,
-            {new: true}
-         ).select("-password")
-     return res
-     .status(200)
-     .json(
-        new  apiResponse( 200, user, "Cover Image updated sucessfully ");
+                coverImage: coverImage.url
+            }
+        },
+        { new: true }
+    ).select("-password")
+    return res
+        .status(200)
+        .json(
+            new apiResponse(200, user, "Cover Image updated sucessfully ");
      )
 
     })
+
+// For watch History   from  one copy from modal to another ...  ..
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+    const { username } = req.params
+    if (!username?.trim()) {
+        throw new apiError(400, "username is missing");
+    }
+    //  aggregation  mongodb pipeline wala part       
+    const channel = await User.aggregate([
+        {
+            $match: {
+                username: username?.toLowerCase()
+            }
+        },
+
+
+        {
+            $lookup: {
+                from: "subscription",   // The collection to join sunsriptions
+                localField: "_id",   //  The field in subsription to  match
+                foreignField: "channel",  // from where should we matched to get the subscription model...
+                as: "subcribers"   //  or What we called  our  as  subscribers.
+            }
+        },
+
+        {
+            /// <important>   for pipeline wala part  its mentioned that  it shoul be in lower case
+            $lookup: {
+                from: "subscription",   // The collection to join sunsriptions
+                localField: "_id",   //  The field in subsription to  match
+                foreignField: "subscriber",  // for calculating    to whom we subscribed  we have to  match the foreingField to subsrbier.
+                as: "subcribedTO"
+            }
+        },
+        {
+            $addFields: {     /// basically it creates or add a fiels  so that we add  up the following..    
+                subscribersCount: {
+                    $size: "$subcribers"
+                },
+                channelSubscribedToCount: {
+                    $size: "$subcribedTO"
+                },
+                isSubscribed: {
+                    $cond: {
+                        if: { $in: [req.User?._id, "$subscribers.subscriber"] },
+                        then: true,
+                        else: false
+                    }
+                }
+
+            }
+
+        },
+        {
+            $project: {
+                fullName: 1,
+                username: 1,
+                subscribersCount: 1,
+                channelSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1
+            }
+        }
+
+    ])
+    if(!channel?.length) {
+        throw new apiError( 404, "channel does not exist");
+    }
+
+    return  res 
+    .status(200)
+    .json(
+        new apiResponse(200, channel[0] ," User channel fetched") 
+    )
+})
 export {
     registerUser,
     loginUser,
     logOutUser,
-    refreshAccessToken ,
-    changePassword ,
+    refreshAccessToken,
+    changePassword,
     getCurrentUser,
     updateAccountDetails,
-    updateUserAvatar, 
+    updateUserAvatar,
     updateCoverImage,
 
 
--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -15,3 +15,5 @@ const subscriptionSchema = new Schema(
 );
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+  
